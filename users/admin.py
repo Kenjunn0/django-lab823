30078f657a91657f3e502014c0375f208dbc44f3
minheonjun <tamunavollt1@gmail.com>
2023-09-03 22:24:08 +0900

    models1

--- users/admin.py
+++ users/admin.py
@@ -3,5 +3,29 @@ from django.contrib.auth.admin import UserAdmin
 from .models import User
 @admin.register(User)
 class CustomUserAdmin(UserAdmin):
-    pass
+    fieldsets = (
+        (
+            "Profile",
+            {
+                "fields": ("profile_photo", "name", "email", "gender", "password", "is_host", "currency", "language", ),
+                "classes": ("wide",)
+            },
+
+        ),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+                "classes": ("collapse",)
+            },
+        ),
+        ("Important dates", {"fields": ("last_login", "date_joined"), "classes": ("collapse",)}),
+    )
+    list_display = ("username", "email", "name", "is_host")
 
--- users/models.py
+++ users/models.py
@@ -1,5 +1,26 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
 class User(AbstractUser):
-    pass
+    class GenderChoices(models.TextChoices):
+        MALE = ("male", "Male")
+        FEMALE = ("female", "FEMALE")
+
+    class LanguageChoices(models.TextChoices):
+        KR = ("kr", "Korean")
+        EN = ("en", "English")
+
+    class CurrencyChoices(models.TextChoices):
+        # 소괄호 없이 작성해도 Tuple로 인식할 수 있다.
+        KRW = "krw", "Korea, KRW"
+        USD = "usd", "USA, USD"
+
+    first_name = models.CharField(max_length=150, editable=False, )
+    last_name = models.CharField(max_length=150, editable=False, )
+    name = models.CharField(max_length=150, default="")
+    profile_photo = models.ImageField(blank=True)
+    gender = models.CharField(max_length=10, choices=GenderChoices.choices, )
+    is_host = models.BooleanField(default=False)
+    language = models.CharField(max_length=2, choices=LanguageChoices.choices, )
+    currency = models.CharField(max_length=5, choices=CurrencyChoices.choices, )
