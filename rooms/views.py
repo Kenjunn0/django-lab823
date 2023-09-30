62c32020193178000ac8c441dd0649ae53f50e5b
minheonjun <tamunavollt1@gmail.com>
2023-09-30 21:07:49 +0900

    render

--- rooms/views.py
+++ rooms/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Room
 
 
 def see_all_rooms(request):
-    return HttpResponse("see all rooms")
+    rooms = Room.objects.all()
+    return render(request, "all_rooms.html", {
+                      'rooms': rooms,
+                      'title': "Hello! this title comes from django!",
+                  })
 
 
 def see_one_room(request, room_id):
-    return HttpResponse(f"see room with id : {room_id}")
+  try:
+    room = Room.objects.get(pk=room_id)
+    return render(request, "room_detail.html",
+                  {
+                    'room': room,
+                  },
+                 )
+  except Room.DoesNotExist:
+    return render(request, "room_detail.html",
+                  {
+                    "not_found": True,
+                  },
+                 )
