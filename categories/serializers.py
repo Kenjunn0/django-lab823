bc8d6a4f638f3d0fa7e16b7d2eceba011b96ded4
minheonjun <tamunavollt1@gmail.com>
2023-12-16 22:48:06 +0900

    update

--- categories/serializers.py
+++ categories/serializers.py
@@ -1,8 +1,9 @@
 from rest_framework import serializers
+from .models import Category
 
 class CategorySerializer(serializers.Serializer):
 
     id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField(max_length=50, required=True)
-    kind = serializers.CharField(max_length=15, required=True)
-    created_at = serializers.DateTimeField(read_only=True)
+    name = serializers.CharField(max_length=50, required=True, )
+    kind = serializers.ChoiceField(choices=Category.CategoryKindChoices.choices, )
+    created_at = serializers.DateTimeField(read_only=True, )
--- categories/views.py
+++ categories/views.py
@@ -1,19 +1,42 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.exceptions import NotFound
 from .models import Category
 from .serializers import CategorySerializer
 
 @api_view(["GET", "POST"])
 def all_categories(request):
-    if (request.method == "GET"):
-        serializer  = CategorySerializer(Category.objects.all(), many=True)
+    if request.method == "GET":
+        serializer = CategorySerializer(Category.objects.all(), many=True)
         return Response({ "Res" : serializer.data })
-    elif (request.method == "POST"):
+    elif request.method == "POST":
         serializer = CategorySerializer(data=request.data)
-        Category.objects.create(name=request.data["name"], kind=request.data["kind"])
+        if serializer.is_valid():
+            serializer.save()
+            return Response({ "Res" : serializer.data })
 
 
-@api_view(["GET", "POST"])
+@api_view(["GET", "PUT"])
 def category(request, pk):
-    serializer = CategorySerializer(Category.objects.get(pk=pk))
-    return Response({ "Res" : serializer.data })
+    try:
+        category = Category.objects.get(pk=pk)
+    except Category.DoesNotExist:
+        raise NotFound
+
+    if request.method == "GET":
+        serializer = CategorySerializer(category)
+        return Response({"Res": serializer.data})
+    elif request.method == "PUT":
+        serializer = CategorySerializer(category, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({ "Res" : serializer.data })
+
+def create(self, validated_data):
+    return Category.objects.create(**validated_data)
+
+def update(self, instance, validated_data):
+    instance.name = validated_data.get("name", instance.name)
+    instance.kind = validated_data.get("kind", instance.kind)
+    instance.save()
+    return instance
