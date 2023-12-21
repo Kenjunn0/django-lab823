2c07794c6425341d2e2c9f46e55fedddad425306
minheonjun <tamunavollt1@gmail.com>
2023-12-22 00:06:40 +0900

    Add : amenity views

--- rooms/views.py
+++ rooms/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Room
+from rest_framework.exceptions import NotFound
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.status import HTTP_204_NO_CONTENT
+
+from .models import Room, Amenity
+from .serializers import AmenitySerializer
 
 
 def see_all_rooms(request):
@@ -9,7 +15,6 @@ def see_all_rooms(request):
                       'rooms': rooms,
                       'title': "Hello! this title comes from django!",
                   })
-
 
 def see_one_room(request, room_id):
   try:
@@ -25,3 +30,44 @@ def see_one_room(request, room_id):
                     "not_found": True,
                   },
                  )
+
+class Amenities(APIView):
+    def get(self, request):
+        amenities = Amenity.objects.all()
+        serializer = AmenitySerializer(amenities, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = AmenitySerializer(data=request.data)
+        if serializer.is_valid():
+            new_amenity = serializer.save()
+            return Response(AmenitySerializer(new_amenity).data)
+        else:
+            return Response(serializer.errors)
+
+class AmenityDetail(APIView):
+
+    def get_object(self, pk):
+        try:
+            return Amenity.objects.get(pk=pk)
+        except Amenity.DoesNotExist:
+            raise NotFound
+
+
+    def get(self, request, pk):
+        return Response(
+            AmenitySerializer(self.get_object(pk)).data
+        )
+
+    def put(self, request, pk):
+        amenity = self.get_object(pk)
+        serializer = AmenitySerializer(amenity, data=request.data, partial=True)
+        if serializer.is_valid():
+            updated_amenity = serializer.save()
+            return Response(AmenitySerializer(updated_amenity).data)
+        else:
+            return Response(serializer.errors)
+
+    def delete(self, request, pk):
+        self.get_object(pk).delete()
+        return Response(HTTP_204_NO_CONTENT)
