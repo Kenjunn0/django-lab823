371e0cc7a7484061bedc4cf9e8b0482d2011e596
minheonjun <tamunavollt1@gmail.com>
2024-03-11 22:34:22 +0900

    add : login logout

--- users/views.py
+++ users/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import authenticate, login, logout
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
@@ -73,7 +74,7 @@ class ChangePassword(APIView):
         user = request.user
         old_password = request.data.get('old_password')
         new_password = request.data.get('new_password')
-        if not old_password or new_password:
+        if not (old_password or new_password):
             raise exceptions.ParseError
         if user.check_password(old_password):
             user.set_password(new_password)
@@ -81,3 +82,30 @@ class ChangePassword(APIView):
             return Response(status=status.HTTP_200_OK)
         else:
             return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+class LogIn(APIView):
+
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        if not (username or password):
+            raise exceptions.ParseError
+        user = authenticate(
+            request,
+            username=username,
+            password=password,
+        )
+        if user:
+            login(request, user)
+            return Response({"ok": "welcome"})
+        else:
+            return Response({"error": "wrong password"})
+
+class LogOut(APIView):
+
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        logout(request)
+        return Response({"ok" : "bye!"})
