7d8a6910d66ecda80a2c69727af29ab640177c83
minheonjun <tamunavollt1@gmail.com>
2024-03-11 20:48:29 +0900

    add : create user

--- users/views.py
+++ users/views.py
@@ -2,6 +2,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from rest_framework import status
+from rest_framework import exceptions
 from . import serializers
 
 class Me(APIView):
@@ -26,4 +27,27 @@ class Me(APIView):
             return Response(serializer.data)
         else:
             return Response(serializer.errors)
+
+
+class User(APIView):
+
+    def post(self, request):
+        password = request.data.get['password']
+        if not password:
+            raise exceptions.ParseError
+
+        user = request.data
+        serializer = serializers.PrivateUserSerializer(data=user)
+        if serializer.is_valid():
+            user = serializer.save()
+            user.set_password(password)
+            user.save()
+            serializer = serializers.PrivateUserSerializer(user)
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+
+
+
+
 
