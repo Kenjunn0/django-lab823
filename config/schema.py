91207631f00d1e6e6ef65d2551db531006ed8587
minheonjun <tamunavollt1@gmail.com>
2024-03-21 16:32:39 +0900

    add : refactor

--- config/schema.py
+++ config/schema.py
@@ -11,25 +11,28 @@ class Movie:
 
 movies_db = [Movie(id=1, title="Gotfather", year=1990, rating=10), ]
 
+def movies():
+    return movies_db
+
+def movie(movie_id:int):
+    return movies_db[movie_id - 1]
+
+def add_movie(title:str, year:int, rating:int):
+    new_movie = Movie(id=len(movies_db) + 1, title=title, year=year, rating=rating)
+    movies_db.append(new_movie)
+    return new_movie
+
 
 @strawberry.type
 class Query:
-    @strawberry.field
-    def movies(self) -> list[Movie]:
-        return movies_db
-
-    @strawberry.field
-    def movie(self, movie_id:int) -> Movie:
-        return movies_db[movie_id - 1]
+    movies: list[Movie] = strawberry.field(resolver=movies)
+    movie: Movie = strawberry.field(resolver=movie)
 
 
 @strawberry.type
 class Mutation:
-    @strawberry.mutation
-    def add_movie(self, title:str, year:int, rating:int) -> Movie:
-        new_movie = Movie(id=len(movies_db) + 1, title=title, year=year, rating=rating)
-        movies_db.append(new_movie)
-        return new_movie
+    add_movie: Movie = strawberry.mutation(resolver=add_movie)
+
 
 
 schema = strawberry.Schema(query=Query, mutation=Mutation)
