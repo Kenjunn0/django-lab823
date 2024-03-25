97ad08e874c2bae99b8ea3eb3232ea0b18a4c358
minheonjun <tamunavollt1@gmail.com>
2024-03-25 22:38:58 +0900

    add : token authentication

--- config/settings.py
+++ config/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 
 THIRD_PARTY_APPS = [
     "rest_framework",
+    "rest_framework.authtoken",
     "strawberry.django",
 ]
 
@@ -155,5 +156,6 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication',
         'config.authentication.TrustMeBroAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
     ]
 }
