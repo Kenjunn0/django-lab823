9a3552cd97625c52ca52fe15472f6a8b7684d5b1
minheonjun <tamunavollt1@gmail.com>
2024-03-27 15:02:22 +0900

    add : amenities test

--- rooms/tests.py
+++ rooms/tests.py
@@ -1,8 +1,23 @@
 # 사용하지 않음
 # from django.test import TestCase
 from rest_framework.test import APITestCase
+from . import models
 
 class TestAmenities(APITestCase):
 
-    def test_two_plus_two(self):
-        self.assertEquals(2+2, 4, "there is a mistake")
+    NAME = "Amenities Name"
+    DESCRIPTION = "Amenities Description"
+
+    def setUp(self):
+        models.Amenity.objects.create(
+            name=self.NAME,
+            description=self.DESCRIPTION
+        )
+
+    def test_all_amenities(self):
+        response = self.client.get("/api/v1/rooms/amenities")
+        data = response.json()
+        self.assertEquals(response.status_code, 200, "status_code isn't 200")
+        self.assertIsInstance(data, list, )
+        self.assertEquals(len(data), 1, )
+        self.assertEquals(data[0]['name'], self.NAME, )
