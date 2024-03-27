22db3f05c23228d63fea1043722b511ae674667c
minheonjun <tamunavollt1@gmail.com>
2024-03-27 13:59:19 +0900

    add : first test

--- rooms/tests.py
+++ rooms/tests.py
@@ -1,3 +1,8 @@
-from django.test import TestCase
+# 사용하지 않음
+# from django.test import TestCase
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+class TestAmenities(APITestCase):
+
+    def test_two_plus_two(self):
+        self.assertEquals(2+2, 4, "there is a mistake")
