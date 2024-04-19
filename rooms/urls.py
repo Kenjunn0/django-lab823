f08f5e70dd2c22c396b70c8801156adfd3203071
minheonjun <tamunavollt1@gmail.com>
2024-04-19 15:34:03 +0900

    add Booking Dates

--- rooms/urls.py
+++ rooms/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import Amenities, AmenityDetail, RoomBookings, Rooms, RoomDetail, RoomReviews, RoomPhotos
+from .views import Amenities, AmenityDetail, RoomBookings, Rooms, RoomDetail, RoomReviews, RoomPhotos, RoomBookingCheck
 
 urlpatterns = [
   path("", Rooms.as_view()),
@@ -7,6 +7,7 @@ urlpatterns = [
   path("<int:pk>/reviews", RoomReviews.as_view()),
   path("<int:pk>/photos", RoomPhotos.as_view()),
   path("<int:pk>/bookings", RoomBookings.as_view()),
+  path("<int:pk>/bookings/check", RoomBookingCheck.as_view()),
   path("amenities", Amenities.as_view()),
   path("amenities/<int:pk>", AmenityDetail.as_view()),
 ]
