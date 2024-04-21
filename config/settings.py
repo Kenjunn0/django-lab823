73d517244540efac47ca1c6baa1f1b1230df05e9
minheonjun <tamunavollt1@gmail.com>
2024-04-21 15:53:24 +0900

    add : render15

--- config/settings.py
+++ config/settings.py
@@ -35,7 +35,7 @@ CF_TOKEN = env("CF_TOKEN")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = "RENDER" not in os.environ
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost",]
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
